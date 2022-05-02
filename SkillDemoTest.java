317994c472a7e86439254500f02f5b1cbdde7856
OrlandoFang <orlandofang0511@gmail.com>
2022-05-01 22:24:36 -0700

    fix tester

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void tester(){
-        assertEquals("HelloWorld", SkillDemo.helloworld());
+        assertEquals("Hello World", SkillDemo.helloworld());
     }
 }
